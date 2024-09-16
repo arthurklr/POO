5d2dded06748f4beda5617ca45fa67348dc7951b
arthurklr_ <arthur.k2104@gmail.com>
2024-09-16 20:59:21 +0200

    Update exemple2.php

--- exemple2.php
+++ exemple2.php
@@ -5,10 +5,10 @@ require "maitre.php";
 $tintin = new Maitre("Tintin", "journaliste", "Belgique", "Milou", "fox-terrier");
 $asterix = new Maitre("Asterix", "guerrier", "Gaules", "", "");
 
-$luckyLuke = new Maitre("Lucky Luke", "cowboy", "Amérique");
+/*$luckyLuke = new Maitre("Lucky Luke", "cowboy", "Amérique");
 echo $luckyLuke->details();
 $luckyLuke->affecter("Rantanplan", "toutou");
-echo $luckyLuke->details();
+echo $luckyLuke->details();*/
 
 echo $tintin->details();
 echo $asterix->details();
