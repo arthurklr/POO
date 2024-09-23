bb7647610a295d2623476dde0c7a611094a20890
Arthur KIENTZLER <147141390+arthurklr@users.noreply.github.com>
2024-09-23 08:43:01 +0200

    Update exemple2.php

--- exemple2.php
+++ exemple2.php
@@ -1,14 +1,28 @@
 <?php
+require "maitre2.php";
 
-require "maitre.php";
+// Instanciation de la classe Chien : $milou est un objet de la classe "Chien"
+// La classe Chien est héritée par la classe Maitre2
+$milou = new Chien("Milou", "caniche");
 
-$tintin = new Maitre("Tintin", "journaliste", "Belgique", "Milou", "fox-terrier");
-$asterix = new Maitre("Asterix", "guerrier", "Gaules", "", "");
+var_dump($milou);
 
-/*$luckyLuke = new Maitre("Lucky Luke", "cowboy", "Amérique");
+echo "<div>".$milou->nomChien."</div>";       // Affichage de l'attribut "nomChien"
+echo "<div>".$milou->race."</div>";           // Affichage de l'attribut "race"
+
+$milou->race = "fox-terrier";                 // Modification de l'attribut "race"
+
+echo $milou->description();                   // Affichage de la méthode "description"
+
+// Instanciation de la classe Maitre2 : $luckyLuke est un objet de la classe "Maitre2"
+// Lucky Luke est le maitre du chien Rantanplan
+$luckyLuke = new Maitre2("Lucky Luke", "cowboy", "Amérique", "Rantanplan", "toutou");
 echo $luckyLuke->details();
-$luckyLuke->affecter("Rantanplan", "toutou");
-echo $luckyLuke->details();*/
 
-echo $tintin->details();
+// Instanciation de la classe Maitre2 : $asterix est un objet de la classe "Maitre2"
+// Astérix ne possède pas de chien (seuls 3 paramètres sont indiqués lors de l'instanciation)
+$asterix = new Maitre2("Astérix", "guerrier", "Gaules");
 echo $asterix->details();
+
+$milou->changerRace("Berger Australien");
+echo $milou->description();
